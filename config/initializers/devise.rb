fcc8ca05f0cdfaa2ca90615c43ce6ac607c7963b
kenney-g <goadke@g.cofc.edu>
2020-11-11 15:32:46 -0500

    added topic resource, edited devise to use omniauth, added bootstrap

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+   config.omniauth :github, '8d3f4a4ee194e489b7c0', '6fb8ece5bea11ac0cb676e99a664cd019753f9ca', scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
