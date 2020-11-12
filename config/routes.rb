de84630c896243cede63981d25edd503a1ceee25
kenney-g <goadke@g.cofc.edu>
2020-11-12 12:34:36 -0500

    devise base and views install inc home controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  #change the basic url to be home/index
+  root to: "home#index"
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
