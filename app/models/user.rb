d9747bbc07c88279db2f7e1204cdb6df91e71b12
kenney-g <goadke@g.cofc.edu>
2020-11-13 17:37:43 -0500

    add omniauth callbacks controller, generate devise user files, add auth callback from omniauth wiki, remove extra links from layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
+         before_action :authenticate_user!
+
+         @user = User.from_omniauth(request.env["omniauth.auth"])
+
 end
