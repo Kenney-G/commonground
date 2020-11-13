fcf4633724b978f16bc46b426bd8e1759cad7ecc
kenney-g <goadke@g.cofc.edu>
2020-11-12 21:13:15 -0500

    add basic devise logic to application controller with username field added, create omniauth migration to add to user database, add method in user to allow for username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
+
+         def login
+           @login || self.username || self.email
+         end
+
 end
