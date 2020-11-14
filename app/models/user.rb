1151e6c326fd3b6641097ff3c8f320aef798d241
kenney-g <goadke@g.cofc.edu>
2020-11-13 19:04:48 -0500

    Troubleshooting broken routes, potential spring gem issue, added omniauth migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
-         before_action :authenticate_user!
-
-         @user = User.from_omniauth(request.env["omniauth.auth"])
-
+         :recoverable, :rememberable, :validatable,
+          :omniauthable, omniauth_providers: [:github]
 end
