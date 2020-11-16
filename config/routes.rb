2fa1064371d940cba13f3a7c5a7c8c416a6c61fc
kenney-g <goadke@g.cofc.edu>
2020-11-16 15:46:43 -0500

    started on routing, added nesting for users/topics/interest_levels, topics/users, interest_levels/topics/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  resources :interest_levels
-  resources :topics
-  root to: "application#home"
-devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :interest_levels
+    resources :topics, shallow: true
+  end
+  resources :topics do 
+      resources :users, only: [:show, :index] do
+      resources :interest_levels
+    end
+  end
+resources :interest_levels do
+  resources :users do
+    
+  end
+end
 
+  end
+  root to: "application#home"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
 end
