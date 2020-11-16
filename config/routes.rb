de07d750b64901e38091afbab1fd67687aa11eba
kenney-g <goadke@g.cofc.edu>
2020-11-16 15:59:01 -0500

    edited routed for simplicity for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,23 @@
 Rails.application.routes.draw do
+
   resources :users do
-    resources :interest_levels
-    resources :topics, shallow: true
-  end
-  resources :topics do 
-      resources :users, only: [:show, :index] do
-      resources :interest_levels
+    resources :topics do
+     resources :interest_levels
     end
   end
-resources :interest_levels do
-  resources :users do
-    
+
+  resources :topics do 
+      resources :users do
+        resources :interest_levels
   end
 end
 
+resources :interest_levels do
+    resources :topics do 
+      resources :users
   end
+end
+
   root to: "application#home"
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
