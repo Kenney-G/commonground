2b55e6bdb649bae5eb6b81267ba28ba57df0fef4
kenney-g <goadke@g.cofc.edu>
2020-11-16 15:11:05 -0500

    added scope method to interest_level

--- app/models/interest_level.rb
+++ app/models/interest_level.rb
@@ -1,6 +1,5 @@
 class InterestLevel < ApplicationRecord
     belongs_to :user
     belongs_to :topic
-
-    
+        scope :recent, -> { where "created_at DESC", :limit => 10)
 end
--- db/migrate/20201114133607_create_interest_levels.rb
+++ db/migrate/20201114133607_create_interest_levels.rb
@@ -1,7 +1,10 @@
 class CreateInterestLevels < ActiveRecord::Migration[6.0]
   def change
     create_table :interest_levels do |t|
-
+      t.belongs_to :user
+      t.belongs_to :topic
+      #user submittable attribute
+      t.text :comment
       t.timestamps
     end
   end
