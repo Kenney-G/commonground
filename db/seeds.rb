c18761d801062e296843d121bbea4ab0b129d485
kenney-g <goadke@g.cofc.edu>
2020-11-16 19:36:54 -0500

    add name and description to topics migration, wrote seeds for topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@ require 'faker'
 10.times do
     User.create(username: Faker::Internet.user_name, email: Faker::Internet.safe_email, encrypted_password: Faker::Internet.password(6, 20, true, true))
 end
+
+Topic.create(name: "Art and culture", description: "Express yourself." )
+Topic.create(name: "Geography and places", description: "'round the World" )
+Topic.create(name: "Health and fitness", description: "An apple a day." )
+Topic.create(name: "History and events", description: "Those who know not their history..." )
+Topic.create(name: "Mathematics and abstractions", description: "Algrabraic! " )
+Topic.create(name: "Natural sciences and nature", description: "Everything below and above the stars" )
+Topic.create(name: "People and self", description: "Hobbies, Visual Arts, Performance Arts etc." )
+Topic.create(name: "Philosophy and thinking", description: "We think, but are we?" )
+Topic.create(name: "Religion and spirituality", description: "Higher power." )
+Topic.create(name: "Social sciences and society", description: "Economics, politics, psychology, education and much much more" )
+Topic.create(name: "Technology and applied sciences", description: "Engineering, computer science, robotics etc." )
+  
+end
