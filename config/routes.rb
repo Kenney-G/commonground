e75559d6d63914a56aecfeee9f8f62325fe98809
kenney-g <goadke@g.cofc.edu>
2020-11-18 18:13:18 -0500

    generated models, controllers, migrations for interests, topics, users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session,
+     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
