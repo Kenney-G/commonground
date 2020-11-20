26b1b231959d53d875703852a3a158596bfea19e
k-k <k-k@Corvid.attlocal.net>
2020-11-20 00:52:57 -0500

    missing file from last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,4 @@
 class UsersController < ApplicationController
     def index
     end
-    def current_user.username
-        email = current_user.email
-        un = self.email.split(/@/)[0]
-        if User.all.username.includes? == un
-          self.username = self.email
-          else    
-            self.username = un
-          end   
-        end    
-
 end
